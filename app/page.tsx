9a37997abe04d2e2b4c685f523e7260a19374dc3
ifshad <shoronshark@gmail.com>
2024-06-25 23:41:29 +0600

    brands and why choose us section added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,100 @@ import Image from "next/image";
 import laptop from "@/public/Images/dmitry-chernyshov-mP7aPSUm7aE-unsplash-removebg-preview.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Footer from "@/components/layout/Footer";
+import { FaLock, FaMessage, FaRecycle, FaShield, FaTag, FaTruck, FaUpload } from "react-icons/fa6";
+import Marquee from "react-fast-marquee";
+import samsung from "@/public/Images/samsung-logo.png";
+import dell from "@/public/Images/dell-logo.png";
+import hp from "@/public/Images/hp-logo.png";
+import lenovo from "@/public/Images/lenovo-logo.png";
+import apple from "@/public/Images/apple-logo.jpg";
+import asus from "@/public/Images/asus-logo.png";
+import ms from "@/public/Images/ms-logo.jpg";
+import toshiba from "@/public/Images/toshiba-logo.png";
+import sony from "@/public/Images/vaio-logo.jpg";
+import compaq from "@/public/Images/compaq-logo.png";
+
+const brandLogos = [
+  {
+    image: samsung,
+    alt: "samsung",
+  },
+  {
+    image: dell,
+    alt: "dell",
+  },
+  {
+    image: hp,
+    alt: "hp",
+  },
+  {
+    image: lenovo,
+    alt: "lenovo",
+  },
+  {
+    image: apple,
+    alt: "apple",
+  },
+  {
+    image: asus,
+    alt: "asus",
+  },
+  {
+    image: ms,
+    alt: "ms",
+  },
+  {
+    image: toshiba,
+    alt: "toshiba",
+  },
+  {
+    image: sony,
+    alt: "sony",
+  },
+  {
+    image: compaq,
+    alt: "compaq",
+  },
+];
+
+const whyChooseUs = [
+  {
+    icon: <FaShield />,
+    title: "Unmatched Quality",
+    description:
+      "We offer top-of-the-line electronics from trusted brands, ensuring you receive products that are reliable and built to last.",
+  },
+  {
+    icon: <FaTag />,
+    title: "Best Prices",
+    description:
+      "Enjoy competitive pricing and exclusive discounts on all our products. Get the best value for your money with TechDynasty.",
+  },
+  {
+    icon: <FaTruck />,
+    title: "Fast Shipping",
+    description:
+      "Experience quick and reliable delivery. We ensure your orders reach you in perfect condition and in the shortest time possible.",
+  },
+  {
+    icon: <FaMessage />,
+    title: "Exceptional Support",
+    description:
+      "Our dedicated support team is here to assist you 24/7. From pre-purchase inquiries to after-sales service, we’ve got you covered.",
+  },
+  {
+    icon: <FaRecycle />,
+    title: "Easy Returns",
+    description:
+      "Shop with confidence with our hassle-free return policy. If you're not satisfied, returning your purchase is quick and straightforward.",
+  },
+  {
+    icon: <FaLock />,
+    title: "Secure Shopping",
+    description:
+      "Your privacy and security are our top priorities. Shop safely with our advanced security measures and encrypted transactions.",
+  },
+];
 
 export default function Home() {
   return (
@@ -30,17 +123,44 @@ export default function Home() {
           <Image src={laptop} alt="tech dynasty laptop"></Image>
         </div>
       </div>
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
-      <Footer />
+      {/* Brand Marquee */}
+      <div className="py-7 md:py-10 lg:py-16">
+        <h1 className="text-center text-2xl md:text-4xl lg:text-5xl font-bold mb-5 md:mb-16">
+          Available Brands
+        </h1>
+        <Marquee pauseOnHover={true} gradient={true} gradientWidth={100}>
+          {brandLogos.map((logo, index) => (
+            <Image
+              key={index}
+              src={logo.image}
+              alt={logo.alt}
+              height={100}
+              className="mx-1"
+            ></Image>
+          ))}
+        </Marquee>
+      </div>
+      {/* Why Choose us */}
+      <div className="container py-7 md:py-10 lg:py-16">
+        <h2 className="text-center text-2xl md:text-4xl lg:text-5xl font-bold mb-5 md:mb-16 text-body-color">
+          Why choose us?
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
+          {whyChooseUs.map((item, index) => (
+            <div key={index} className="px-10 py-8 rounded-xl bg-white hover:shadow-lg transition duration-500 border">
+              <span className="h-16 w-16 bg-primary/10 rounded-full mb-4 inline-flex items-center justify-center">
+                {item.icon}
+              </span>
+              <h3 className="text-lg md:text-xl font-semibold mb-2">
+                {item.title}
+              </h3>
+              <p className="opacity-80 text-base md:text-lg">
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
