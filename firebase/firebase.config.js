7d0a30c92c7731a3363841a8412026013a9aec84
ifshad <shoronshark@gmail.com>
2024-06-27 01:37:22 +0600

    firebase added

---
+++ firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_6rBgoVhHGk4B7oqDNoWJgAo4jbgcbaU",
+  authDomain: "project-tech-dynasty.firebaseapp.com",
+  projectId: "project-tech-dynasty",
+  storageBucket: "project-tech-dynasty.appspot.com",
+  messagingSenderId: "813144028651",
+  appId: "1:813144028651:web:233286abd22597599c55ae",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
