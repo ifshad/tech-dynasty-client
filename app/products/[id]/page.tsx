0793c5e3b1800287db58a24f8cb4c83d02534a77
ifshad <shoronshark@gmail.com>
2024-06-26 11:28:01 +0600

    buy page created

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import getOneProduct from "@/lib/getOneProduct";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default async function ProductDetailsPage({ params }: any) {
+  const { id } = params;
+  const comment = await getOneProduct(id);
+  const { postId, name, email, body } = comment;
+  console.log(comment);
+  return (
+    <div className="">
+      <h1 className="lg:text-6xl md:text-4xl text-3xl font-semibold lg:m-5 md:m-3 m-2 text-center">
+        Product Detail
+      </h1>
+      <Card className="w-[650px] mx-auto">
+        <CardHeader>
+          <CardTitle>{name}</CardTitle>
+          <CardDescription>{email}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>
+            <span className="font-bold">Comment: </span>
+            {body}
+          </p>
+          <p>
+            <span className="font-bold">PostID: </span>
+            {postId}
+          </p>
+        </CardContent>
+        <CardFooter>
+          <p>
+            <span className="font-bold">ID: </span>
+            {id}
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import getProducts from "@/lib/getProducts";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
-const ProductsPage = () => {
-    return (
-        <div>
-            product catalog here...
+export default async function ProductsPage() {
+  const comments = await getProducts();
+  return (
+    <div>
+      <div>
+        <h1 className="lg:text-6xl md:text-4xl text-3xl font-semibold lg:m-5 md:m-3 m-2 text-center">
+          Featured Products
+        </h1>
+        <div className="grid grid-cols-3 gap-4">
+          {comments.map((comment: any) => (
+            <Card key={comment.id} className="w-[350px]">
+              <CardHeader>
+                <CardTitle>{comment.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  {comment.body.length > 60
+                    ? `${comment.body.substring(0, 60)}...`
+                    : comment.body}
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Button>
+                  <Link href={`/products/${comment.id}`}>Details</Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
-    );
-};
-
-export default ProductsPage;
+      </div>
+    </div>
+  );
+}
