9a2f998f55d0b10c86e5df93a59c9a482615325d
ifshad <shoronshark@gmail.com>
2024-06-29 13:57:39 +0600

    added force dynamic for productspage

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -20,6 +20,8 @@ import {
 } from "@/components/ui/tooltip";
 import { FileWarningIcon } from "lucide-react";
 
+export const dynamic = 'force-dynamic';
+
 export default async function ProductsPage() {
   const products = await getProducts();
   return (
